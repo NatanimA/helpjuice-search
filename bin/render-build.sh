9e178fb34285e61b38ae30375ca62ffcb8fbf1de
NatanimA <natanimabesha@gmail.com>
2025-04-16 11:16:14 +0300

    Configure for Render free tier using SQLite

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,9 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+# Create SQLite database directory if it doesn't exist
+mkdir -p db
+
+# Run migrations
 bundle exec rake db:migrate 
