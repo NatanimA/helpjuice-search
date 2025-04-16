a58b3e44cbea604c57e4301e7ee975afe06e9523
NatanimA <natanimabesha@gmail.com>
2025-04-16 11:38:42 +0300

    Fix build issues for Render deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
+# Ensure bundler is not frozen
+bundle config set --local frozen false
+
+# Install dependencies
 bundle install
+
+# Asset compilation
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
